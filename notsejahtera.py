18ac32cebb4d01db60d2c3f2a0b861cd9d86b1a5
leeyep <leeyep325@gmail.com>
2021-11-07 02:00:42 +0800

    added risk level

--- notsejahtera.py
+++ notsejahtera.py
@@ -22,7 +22,7 @@
 from tkinter import*
 from tkinter import messagebox
 
-#logout - exit program
+#frame1
 def showframe1():
     exit()
 
@@ -468,6 +468,12 @@ vac_list = Listbox(frame8,height=20,width=50,selectmode=EXTENDED,listvariable=va
 for i in range(len(Vac_centre)):
     vac_list.insert(i,(Vac_centre[i]))
 
+# scrollbar
+vac_listS = Scrollbar(frame8)
+vac_listS.pack(side=RIGHT,fill=Y)
+vac_list.config(yscrollcommand=vac_listS.set)
+vac_listS.config(command=vac_list.yview)
+
 
 add_button = Button(frame8,text='Add vaccination centre',command=lambda: add())
 remove_button = Button(frame8,text='Remove vaccination centre',command=lambda: remove())
@@ -486,6 +492,7 @@ space2.pack()
 Button(frame8,text='Back',command=lambda:show_frame(frame10)).pack()
 
 # assign shit
+
 def assign_user():
     openVaccination = open("vaccination.txt","a")
     x = user_list.get(user_list.curselection()) +', '+ vac_list2.get(vac_list2.curselection())
@@ -493,12 +500,29 @@ def assign_user():
     openVaccination.write(x + '\n')
     openVaccination.close()
 
+def remove_user():
+    assigned_user.delete(END,assigned_user.curselection())
     
-
-
 user_list=Listbox(frame9,height=30,width=60,exportselection=0)
 vac_list2 = Listbox(frame9,height=30,width=60,exportselection=0,listvariable=var1)
 assigned_user=Listbox(frame9,height=30,width=60)
+
+# scrollbar
+user_listS = Scrollbar(frame9)
+user_listS.grid(row=1,column=2,rowspan=5)
+user_list.config(yscrollcommand=user_listS.set)
+user_listS.config(command=user_list.yview)
+
+vac_list2S = Scrollbar(frame9)
+vac_list2S.grid(row=1,column=4,rowspan=5)
+vac_list2.config(yscrollcommand=vac_list2S.set)
+vac_list2S.config(command=vac_list2.yview)
+
+assigned_userS = Scrollbar(frame9)
+assigned_userS.grid(row=1,column=6,rowspan=5)
+assigned_user.config(yscrollcommand=assigned_userS.set)
+assigned_userS.config(command=assigned_user.yview)
+
 
 user_details = list(open('user_personalinfo.txt').readlines())
 for line in user_details:
@@ -509,22 +533,36 @@ for line in user_details:
     d = d.strip() #age
     e = e.strip() #phone number
     f = f.strip() #postcode
-    g = g.strip() #occupation
+    g = g.strip().lower() #occupation
     h = h.strip() #chronic disease status
-    user_list.insert(END,f'{c}, {d}, {g}, {a}') ## Problem
+    if g =='doctor'or g =='nurse' or g=='soldier' :
+        risk_level = 5
+    elif g=='teacher' or g=='lecturer' or g =='student' or g=='politician':
+        risk_level = 4
+    elif g=='motorsport driver' or g=='chef':
+        risk_level = 3
+    elif g=='ship technician':
+        risk_level = 2
+    elif g=='unemployed' or g=='youtuber' or g=='software engineer':
+        risk_level = 1
+    else:
+        risk_level = 'Unknown risk, run away immedi-'
+    user_list.insert(END,f'{c}, {d}, {g}, {risk_level}')
 
 
 assign_button= Button(frame9,text='Assign',command=assign_user)
+removeuser_button= Button(frame9,text='Assign',command=remove_user)
 
 # empty space
 Label(frame9,text='').grid(row=0)
 Label(frame9,text='').grid(row=1,column=0,padx=90)
-Button(frame9,text='Back',command=lambda:show_frame(frame10)).grid(row=3,column=2)
 # not empty space
+Button(frame9,text='Back',command=lambda:show_frame(frame10)).grid(row=5,column=3)
 user_list.grid(row=1,column=1,padx=10,pady=10)
-vac_list2.grid(row=1,column=2,padx=10,pady=10)
-assigned_user.grid(row=1,column=3,padx=10,pady=10)
-assign_button.grid(row=2,column=2)
+vac_list2.grid(row=1,column=3,padx=10,pady=10)
+assigned_user.grid(row=1,column=5,padx=10,pady=10)
+assign_button.grid(row=2,column=3)
+removeuser_button.grid(row=3,column=3)
 
 
 #admin menu
